7820cfd3cbb3faa89f322c98ba299c9dcca5d8c7
Kieran Gainer <86863603+krgainer@users.noreply.github.com>
2023-07-14 20:33:18 +0200

    Update to only save 10 digits after the decimal

--- Eulers.py
+++ Eulers.py
@@ -6,6 +6,17 @@ from math import sin, pi, exp
 def derivativeFunction(t, y):
     return t**2 * y + sin(2*pi*t)
 
+# Define the different step sizes to be used in Euler's method
+h_values = [0.1, 0.01, 0.001, .0001]
+
+# Define initial conditions, ranges and derivative functions for each problem
+problem_name = 'example'  # Name of the problem, for saving purposes
+problem = {"y0": 1, "t0": 0, "tf": 2, "dydt": derivativeFunction}  # Problem parameters
+
+
+# --------------------------------------------
+
+
 # Define Euler's method for numerical integration
 def euler_method(dydt, y0, t0, tf, h):
     results = []  # Initialize the output string
@@ -13,16 +24,9 @@ def euler_method(dydt, y0, t0, tf, h):
     t = t0  # Initialize t with the start time
     while t <= tf:  # Loop until end time is reached
         y = y + h * dydt(t, y)  # Update y using Euler's method
-        results.append({"t": t, "y": y})  # Store the current time and y value    
+        results.append({"t": format(t, '.10f'), "y": format(y, '.10f')})  # Store the current time and y value, to 10 decimal places for both.
         t += h  # Increment the time by the step size h
     return results  # Return the results list
-
-# Define the different step sizes to be used in Euler's method
-h_values = [0.1, 0.01, 0.001, .0001]
-
-# Define initial conditions, ranges and derivative functions for each problem
-problem_name = 'example'  # Name of the problem, for saving purposes
-problem = {"y0": 1, "t0": 0, "tf": 2, "dydt": derivativeFunction}  # Problem parameters
 
 # Solve the problemS using Euler's method
 for h in h_values:  # Loop over the different step size
